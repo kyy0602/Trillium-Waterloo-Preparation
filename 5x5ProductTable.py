c0d45ef804743b49c13f36eb891ac50a22864573
kyy0602 <kyy0602@outlook.com>
2017-12-20 17:28:21 -0500

    Change to rectangle

--- 5x5ProductTable.py
+++ 5x5ProductTable.py
@@ -1,5 +1,8 @@
 n=5
 for x in range(1,6):
     for o in range(x,n*x+1,x):
-        print(o,end=' ')
+        if o<10:
+            print(o,end='  ')
+        else:
+            print(o,end=' ')
     print(end='\n')
