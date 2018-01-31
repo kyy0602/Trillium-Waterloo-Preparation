f320283307108eaec65198cfa9c534055298246c
Scarlett Kang <kyy0602@outlook.com>
2018-01-30 21:06:14 -0500

    Little changes and Problem Risk

--- 2017Beaver_Skaters.py
+++ 2017Beaver_Skaters.py
@@ -1,14 +1,14 @@
-Row = ["P", "Q", "R", "S", "T", "U", "V"]
-a=0
+ROW = ["P", "Q", "R", "S", "T", "U", "V"]
+A = 0
 
 for x in range(60):
     if x <= 60:
-        a = Row [6]
-        Row [6] = Row [5]
-        Row [5] = Row [4]
-        Row [4] = Row [3]
-        Row [3] = Row [2]
-        Row [2] = Row [1]
-        Row [1] = Row [0]
-        Row [0] = a
-print(Row[6])
+        a = ROW[6]
+        ROW[6] = ROW[5]
+        ROW[5] = ROW[4]
+        ROW[4] = ROW[3]
+        ROW[3] = ROW[2]
+        ROW[2] = ROW[1]
+        ROW[1] = ROW[0]
+        ROW[0] = a
+print(ROW[6])
